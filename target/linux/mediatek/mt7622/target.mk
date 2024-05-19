dd7158d950ba76484bf2d1287d3f0a69a5b65927
mrhpx <hpx502766238@github.com>
2024-05-19 12:16:41 +0800

    add mtkhqos,remove wpad-openssl-mesh

--- target/linux/mediatek/mt7622/target.mk
+++ target/linux/mediatek/mt7622/target.mk
@@ -2,7 +2,8 @@ ARCH:=aarch64
 SUBTARGET:=mt7622
 BOARDNAME:=MT7622
 CPU_TYPE:=cortex-a53
-DEFAULT_PACKAGES += wpad-mesh-openssl uboot-envtools swconfig bridger mtkhqos_util
+#DEFAULT_PACKAGES += wpad-mesh-openssl uboot-envtools swconfig bridger mtkhqos_util
+DEFAULT_PACKAGES += uboot-envtools swconfig bridger mtkhqos_util
 KERNELNAME:=Image dtbs
 
 define Target/Description
